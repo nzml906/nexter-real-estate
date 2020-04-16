0781e18769bedfec7a48dfd19113b3ea0378eb7a
Nazmul <grey000906@gmail.com>
2020-04-16 07:52:22 +0600

    deploy app

---
+++ precache-manifest.42001610ef12ddd13c20fbcecd3a038d.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc14a9a35cbffe6d7848",
+    "url": "/nexter-real-estate/css/app.1cc88ec0.css"
+  },
+  {
+    "revision": "f250e12628a7b2a12cb9141fa2e5cda1",
+    "url": "/nexter-real-estate/img/back.f250e126.jpg"
+  },
+  {
+    "revision": "f250e12628a7b2a12cb9141fa2e5cda1",
+    "url": "/nexter-real-estate/img/back.jpg"
+  },
+  {
+    "revision": "60a963d82b5bd5b32df795062a348a3b",
+    "url": "/nexter-real-estate/img/favicon.png"
+  },
+  {
+    "revision": "581fc6ad45a1428157168bfff7c40e03",
+    "url": "/nexter-real-estate/img/gal-1.jpeg"
+  },
+  {
+    "revision": "9865e700f1cd5e17135a6f75da6b1378",
+    "url": "/nexter-real-estate/img/gal-10.jpeg"
+  },
+  {
+    "revision": "5536a90f3836ebf8085ec0ae46984867",
+    "url": "/nexter-real-estate/img/gal-11.jpeg"
+  },
+  {
+    "revision": "0709a3b0434b69b42cdd096e4d13f90f",
+    "url": "/nexter-real-estate/img/gal-12.jpeg"
+  },
+  {
+    "revision": "b4740c9c0d2cec5640904289867c8ce6",
+    "url": "/nexter-real-estate/img/gal-13.jpeg"
+  },
+  {
+    "revision": "486c8880b61fee96a95de85288a1e6ae",
+    "url": "/nexter-real-estate/img/gal-14.jpeg"
+  },
+  {
+    "revision": "ee7b3ca7ee80e0b5c62391c6ec93a671",
+    "url": "/nexter-real-estate/img/gal-2.jpeg"
+  },
+  {
+    "revision": "628f87aeedf507d437f6aeb12bcc4215",
+    "url": "/nexter-real-estate/img/gal-3.jpeg"
+  },
+  {
+    "revision": "c6fff3a13ac92abd83a80c208c4dda53",
+    "url": "/nexter-real-estate/img/gal-4.jpeg"
+  },
+  {
+    "revision": "4114ded407c6764a037d322452fd6ca0",
+    "url": "/nexter-real-estate/img/gal-5.jpeg"
+  },
+  {
+    "revision": "aa2376b5a04a3bf548d9f1773f580da1",
+    "url": "/nexter-real-estate/img/gal-6.jpeg"
+  },
+  {
+    "revision": "6085a127abe2dab815ab22ec0a9135d1",
+    "url": "/nexter-real-estate/img/gal-7.jpeg"
+  },
+  {
+    "revision": "f30fbda69d5eca2f340eb6d4d093a914",
+    "url": "/nexter-real-estate/img/gal-8.jpeg"
+  },
+  {
+    "revision": "92758fee44f610a3b225c33ff90a9333",
+    "url": "/nexter-real-estate/img/gal-9.jpeg"
+  },
+  {
+    "revision": "d11b666fa6c694c620ff867c6669cf7f",
+    "url": "/nexter-real-estate/img/hero.d11b666f.jpeg"
+  },
+  {
+    "revision": "d11b666fa6c694c620ff867c6669cf7f",
+    "url": "/nexter-real-estate/img/hero.jpeg"
+  },
+  {
+    "revision": "5120ad222b9080b343187d3c51e851f3",
+    "url": "/nexter-real-estate/img/house-1.jpeg"
+  },
+  {
+    "revision": "ed12ff11bf26663331ad01ff18d3ebfb",
+    "url": "/nexter-real-estate/img/house-2.jpeg"
+  },
+  {
+    "revision": "521c065d9e8bc6ff2ec850a3ea58a4bf",
+    "url": "/nexter-real-estate/img/house-3.jpeg"
+  },
+  {
+    "revision": "83ee1d1e0e83198aa12b8a63af3f8a2b",
+    "url": "/nexter-real-estate/img/house-4.jpeg"
+  },
+  {
+    "revision": "721effb4a9ef7f3ac5e281cb13cda239",
+    "url": "/nexter-real-estate/img/house-5.jpeg"
+  },
+  {
+    "revision": "ee021f8ac6ebfadeab35a43e6ef6b306",
+    "url": "/nexter-real-estate/img/house-6.jpeg"
+  },
+  {
+    "revision": "9bedec4b6c08dde18d53264bbeb6de12",
+    "url": "/nexter-real-estate/img/logo-bbc.png"
+  },
+  {
+    "revision": "ee16c7b8a84ac078afc2b46923cc6187",
+    "url": "/nexter-real-estate/img/logo-bi.png"
+  },
+  {
+    "revision": "e5c174a02df208c513cfb932428e84c9",
+    "url": "/nexter-real-estate/img/logo-forbes.png"
+  },
+  {
+    "revision": "a473e3878ca044d42ba86f99b685ba47",
+    "url": "/nexter-real-estate/img/logo-techcrunch.png"
+  },
+  {
+    "revision": "764685dbee21c14ef7a112168e92b429",
+    "url": "/nexter-real-estate/img/logo.png"
+  },
+  {
+    "revision": "406400ead91831b741f5246c50146142",
+    "url": "/nexter-real-estate/img/realtor-1.jpeg"
+  },
+  {
+    "revision": "c11e84dcdad4dbf1cb49ff5813ed6611",
+    "url": "/nexter-real-estate/img/realtor-2.jpeg"
+  },
+  {
+    "revision": "af9ff289ef4aa6e35a09b66ec1e42419",
+    "url": "/nexter-real-estate/img/realtor-3.jpeg"
+  },
+  {
+    "revision": "d666b9f5c418437c69c8b78d4b75f3da",
+    "url": "/nexter-real-estate/img/sprite.svg"
+  },
+  {
+    "revision": "24268b24588bddc750dc3b45bd820459",
+    "url": "/nexter-real-estate/img/story-1.jpeg"
+  },
+  {
+    "revision": "915f2bd1ebf80f9d775aa9954f294269",
+    "url": "/nexter-real-estate/img/story-2.jpeg"
+  },
+  {
+    "revision": "bb9b0bfa3b020fd64c07f589baa1aded",
+    "url": "/nexter-real-estate/img/text.txt"
+  },
+  {
+    "revision": "5aa25aca8a696cfae5abac9892300873",
+    "url": "/nexter-real-estate/index.html"
+  },
+  {
+    "revision": "bc14a9a35cbffe6d7848",
+    "url": "/nexter-real-estate/js/app.f585c0da.js"
+  },
+  {
+    "revision": "1ae143bd0437ae685c14",
+    "url": "/nexter-real-estate/js/chunk-vendors.1f4ca57b.js"
+  },
+  {
+    "revision": "046ea2482f8a9830601db582fa17bae5",
+    "url": "/nexter-real-estate/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nexter-real-estate/robots.txt"
+  }
+]);
